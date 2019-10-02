97aa01646bcbaece67132e2fdd03dc8332889904
Reik Schatz <r_schatz@wargaming.net>
2019-10-02 09:58:43 +0200

    init

---
+++ src/main/kotlin/me/reik/r2dbc/R2dbcApplication.kt
@@ -0,0 +1,11 @@
+package me.reik.r2dbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class R2dbcApplication
+
+fun main(args: Array<String>) {
+	runApplication<R2dbcApplication>(*args)
+}
